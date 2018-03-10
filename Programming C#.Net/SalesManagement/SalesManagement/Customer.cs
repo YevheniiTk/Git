efeeeaca0f4cd1f32574ed7b57e5b219a1b51004
YevheniiTk <vkstrelok@gmail.com>
2018-03-11 00:27:22 +0200

    Add SalesManagement

---
+++ Programming C#.Net/SalesManagement/SalesManagement/Customer.cs
@@ -0,0 +1,21 @@
+﻿namespace SalesManagement
+{
+    using System;
+
+    public class Customer
+    {
+        private string _name;
+
+        public Customer(string name)
+        {
+            Console.WriteLine($"Create new castomer {name}");
+            this._name = name;
+        }
+
+        public void GotNewGoods(object obj, GoodsInfoEventArgs goodsInfoEventArgs)
+        {
+            Console.WriteLine($"Got information about new goods - " +
+                              $"{(obj as OnlineShop).Goods} for {_name}");
+        }
+    }
+}
---
+++ Programming C#.Net/SalesManagement/SalesManagement/OnlineShop.cs
@@ -0,0 +1,24 @@
+﻿namespace SalesManagement
+{
+    using System;
+
+    public class OnlineShop
+    {
+        public OnlineShop()
+        {
+            Console.WriteLine($"Create OnlineShop");
+        }
+
+        public event EventHandler<GoodsInfoEventArgs> EventNewGoods;
+
+        public string Goods { get; private set; }
+
+        public void NewGoods(string goodsName)
+        {
+            this.Goods = goodsName;
+            Console.WriteLine($"Add new goods in Online Shop {goodsName}");
+
+            this.EventNewGoods?.Invoke(this, new GoodsInfoEventArgs(goodsName));
+        }
+    }
+}
